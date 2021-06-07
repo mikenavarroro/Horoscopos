c528ee55a81248754692f91b4cd82e9fb6707e11
mikeavarroro <mike.navarroro@gmail.com>
2021-06-06 23:18:56 -0500

    delete js

--- script/main.js
+++
@@ -1,8 +0,0 @@
-funcion signo(){
-  var cumple = document.querySelector('.cumple');
-
-  var mes = document.querySelector('#mes');
-  var dia = document.querySelector('#dia');
-
-  document.write(mes, dia)
-}
